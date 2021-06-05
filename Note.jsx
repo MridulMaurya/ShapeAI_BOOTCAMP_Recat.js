0f16d12a0f21603e89d43a45f8f5391c93ad16fd
MridulMaurya <85413952+MridulMaurya@users.noreply.github.com>
2021-06-05 12:44:57 -0700

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        The instructor during the session was Mr. Shaurya Sinha (Data Analyst
+        Intern at Jio). I got to learn a lot during these 7 days and it was an
+        amazing experience learning with SHAPEAI.
+      </p>
+    </div>
+  );
+}
+export default Note;
